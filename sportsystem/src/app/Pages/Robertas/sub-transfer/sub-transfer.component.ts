4cf88f09f8d488effff395aab2f41b2e97f936f4
kestutis7g <kestutisgirnius@gmail.com>
2021-12-21 02:46:56 +0200

    belenkoks prenumeratu perdavimas veikia

--- sportsystem/src/app/Pages/Robertas/sub-transfer/sub-transfer.component.ts
+++ sportsystem/src/app/Pages/Robertas/sub-transfer/sub-transfer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api-service';
+import { ISub } from 'src/model/ISub';
 
 @Component({
   selector: 'app-sub-transfer',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubTransferComponent implements OnInit {
 
-  constructor() { }
+  sub: ISub = new ISub()
+  userId: number = 1;
+  constructor(
+    private service: ApiService,
+    private activatedRoute: ActivatedRoute,
+    private route: Router
+  ) { }
 
   ngOnInit(): void {
+    this.service.getSubByUserId(parseInt(localStorage.getItem('userId') || "0")).subscribe(
+      data => {
+        this.sub = data;
+        console.log(this.sub);
+      },
+      error => {
+        console.log(error);
+      }
+    )
   }
 
+  transferSub() {
+    this.sub.userId = this.userId;
+    this.sub.id = 0
+    this.service.addSub(this.sub).subscribe(
+      data => {
+
+      },
+      error => {
+        console.log(error);
+      }
+    )
+
+  }
 }
